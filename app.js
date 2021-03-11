907723c52b6d6d436a9cdbc4e15c660c02ba990b
Gaurav Tajanpure <75622826+gauravtajanpure@users.noreply.github.com>
2021-03-11 11:27:45 +0530

    refactored code
    updated es6 methods

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ let btnTranslate = document.querySelector("button");
 let textInput = document.querySelector("#txt-input");
 let outputMsg = document.querySelector("#output-msg");
 
-var serverURL = "https://cors-anywhere.herokuapp.com/https://api.funtranslations.com/translate/dothraki.json";
+const serverURL = "https://cors-anywhere.herokuapp.com/https://api.funtranslations.com/translate/dothraki.json";
 
-function constructUrl(inputText) {
-  return serverURL + "?" + "text=" + inputText;
-}
+const constructUrl = inputText => serverURL + "?" + "text=" + inputText;
 
-function errorHandler(error) {
+const errorHandler = error => {
   console.log("error occurred", error);
   alert(
     "Error occured! Please try after sometime we will resolve the issue very soon."
   );
 }
 
-function clickHandler() {
-  var inputText = textInput.value;
+const clickHandler = () => {
+  let inputText = textInput.value;
 
   fetch(constructUrl(inputText))
     .then((response) => response.json())
